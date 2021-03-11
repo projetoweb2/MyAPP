a0d462cd480e29f2063bb8ce13d98bb4eea190fa
Rafael Lucas <arquivoweyne@gmail.com>
2021-03-11 16:20:56 -0300

    Baixando e importando a biblioteca slider do expo.io

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+import Slider from '@react-native-community/slider';
 
 export default function App(){
   return(
     <View style={styles.container}>
-      
+      <Image
+      source={require('./src/assets/logo.png')}
+      style={styles.logo}
+      />
+
+      <Text style={styles.title}>12 Caracteres</Text>
+      <View style={styles.area}>
+        <Slider
+           style={{ height: 50 }}
+           minimumValue={5}
+           maximumValue={15}
+        />
+      </View>
     </View>
 
   )
