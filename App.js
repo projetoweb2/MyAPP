b61b281dc5ee431f7ec723ffb884092d36bb4d69
Rafael Lucas <arquivoweyne@gmail.com>
2021-03-11 15:06:23 -0300

    testando o styleseet

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App(){
   return(
-    <View style={ {marginTop: 60 }}>
-      <Text>Meu primeiro App com React-Native</Text>
+    <View style={styles.container}>
+      <Text style={{ fontSize: 25 }}>Meu primeiro App com React-Native</Text>
+      <Text>@rafaellucasdesenvolvedorweb</Text>
     </View>
 
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    marginTop: 100
+  }
+})
